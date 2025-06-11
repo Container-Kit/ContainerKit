88630ecd4449264fdff997e8274b2ca3c46d37e9
theetherGit <meenashivam9650@gmail.com>
2025-06-11 09:50:19 +0530

    Intial Setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { ModeWatcher } from "mode-watcher";
+    import "../app.css";
+
+    import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+    import AppSidebar from "$lib/components/app-sidebar.svelte";
+    import SiteHeader from "$lib/components/site-header.svelte";
+
+    let { children } = $props();
+</script>
+
+<ModeWatcher defaultMode="light" track={true} defaultTheme="tangerine" />
+
+<div class="min-h-screen bg-background w-full">
+    <Sidebar.Provider
+        style="--sidebar-width: calc(var(--spacing) * 72); --header-height: calc(var(--spacing) * 12);"
+    >
+        <AppSidebar variant="inset" />
+        <Sidebar.Inset>
+            <SiteHeader />
+            {@render children()}
+        </Sidebar.Inset>
+    </Sidebar.Provider>
+</div>
